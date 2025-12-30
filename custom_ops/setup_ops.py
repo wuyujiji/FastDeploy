2e2c0ae89f3fdf16389957bd2fa6a0325b185d2f
wujiji <yuzzhewu@163.com>
2025-12-30 11:10:51 +0800

    support fused_neox_rope_embedding

--- custom_ops/setup_ops.py
+++ custom_ops/setup_ops.py
@@ -559,6 +559,7 @@ elif paddle.is_compiled_with_custom_device("iluvatar_gpu"):
                 "gpu_ops/recover_decode_task.cu",
                 "gpu_ops/update_inputs_v1.cu",
                 "gpu_ops/get_img_boundaries.cc",
+                "gpu_ops/fused_neox_rope_embedding.cu",
                 "iluvatar_ops/moe_dispatch.cu",
                 "iluvatar_ops/moe_reduce.cu",
                 "iluvatar_ops/flash_attn_unpadded.cu",
